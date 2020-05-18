bf382bbb1cc6576e9ef761d43a7298e15ef621d6
Stelmaszv <stelmaszv@gmail.com>
2020-05-18 14:32:57 +0200

    add create test get and post

--- crudtdd/crudtdd/crud/tests.py
+++ crudtdd/crudtdd/crud/tests.py
@@ -1,13 +1,16 @@
 from django.test import TestCase,Client
 from django.urls import reverse,resolve
-from .views import List,Get
+from .views import List,Get,Create
 from .models import list_Model
 class TestTemplate(TestCase):
     def setUp(self):
         self.client=Client()
         self.list=reverse('main')
         self.get=reverse('Get', kwargs={"id":1})
+        self.create=reverse('create')
         list_Model.objects.create(name='dqd')
+    def test_create(self):
+        self.assertEquals(resolve(self.create).func.view_class, Create)
     def test_list(self):
         self.assertEquals(resolve(self.list).func.view_class,List)
     def test_get_url(self):
@@ -16,12 +19,20 @@ class TestTemplate(TestCase):
         respanse = self.client.get(self.list)
         self.assertEquals(respanse.status_code,200)
         self.assertTemplateUsed(respanse, 'mian.html')
-    def test_get_status_code(self):
-        respanse = self.client.get(self.get)
-        self.assertEquals(respanse.status_code, 200)
-    def test_list_templete(self):
+    def test_create_templete_status_code_and_Template_get(self):
+        respanse = self.client.get(self.list)
+        self.assertEquals(respanse.status_code,200)
+        self.assertTemplateUsed(respanse, 'create.html')
+    def test_create_templete_status_code_and_Template_post(self):
+        respanse = self.client.post(self.create,{
+            'name':'fqef'
+        })
+        self.assertEquals(reverse.status_code,302)
+        self.assertEquals(len(list_Model.objects.all()),1)
+    def test_get_status_code_and_Template(self):
         respanse = self.client.get(self.get)
         self.assertTemplateUsed(respanse, 'get.html')
+        self.assertEquals(respanse.status_code, 200)
 
 
 
