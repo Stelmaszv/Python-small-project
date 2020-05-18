650067703f3d26487d69af1b38a8ece683528891
Stelmaszv <stelmaszv@gmail.com>
2020-05-18 13:35:51 +0200

    add get test

--- crudtdd/crudtdd/crud/tests.py
+++ crudtdd/crudtdd/crud/tests.py
@@ -1,9 +1,12 @@
 from django.test import SimpleTestCase,TestCase
 from django.urls import reverse,resolve
-from .views import List
+from .views import List,Get
 class TestsUrls(SimpleTestCase):
     def test_list(self):
         url = reverse('main')
+        self.assertEquals(resolve(url).func.view_class,List)
+    def test_get(self):
+        url = reverse('get',args=[1])
         self.assertEquals(resolve(url).func.view_class,List)
 class TestTemplate(TestCase):
     def test_list(self):
@@ -11,6 +14,11 @@ class TestTemplate(TestCase):
         respanse = self.client.get(url)
         self.assertEquals(respanse.status_code,200)
         self.assertTemplateUsed(respanse, 'mian.html')
+    def test_get(self):
+        url = reverse('get',args=[1])
+        respanse = self.client.get(url)
+        self.assertEquals(respanse.status_code,200)
+        self.assertTemplateUsed(respanse, 'get.html')
 
 
 
