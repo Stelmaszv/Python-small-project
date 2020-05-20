ab35d4e78db9bfbfb1e72b02e2ad1e7d9570fd15
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 12:31:55 +0200

    add new app urls in crud app

--- Api/main/crud/urls.py
+++ Api/main/crud/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from django.urls import path, include
+from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('list',views.ListView)
+
+urlpatterns = [
+    path('',include(router.urls)),
 ]
