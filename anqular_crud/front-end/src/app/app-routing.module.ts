a2ce0bcc452dfd9ad62dbfffb7f71f0dd1a7e0ed
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 14:51:21 +0200

    add routing

--- anqular_crud/front-end/src/app/app-routing.module.ts
+++ anqular_crud/front-end/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'list' , component:ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[ListComponent]
--- anqular_crud/front-end/src/app/app.module.ts
+++ anqular_crud/front-end/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
