b7808456cf417cfeaf6d97f7caf28f3012e96aad
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 15:18:31 +0200

    add get component

--- anqular_crud/front-end/src/app/app-routing.module.ts
+++ anqular_crud/front-end/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { BaseComponent } from './base/base.component';
 import { AnqularStartComponent } from './anqular-start/anqular-start.component';
 import { ListComponent } from './list/list.component'
 import { CreateComponent } from './create/create.component';
+import { GetComponent } from './get/get.component';
 const routes: Routes = [
   { path:'' , component:ListComponent},
   { path:'create' , component: CreateComponent},
+  { path:'list/:id' , component: GetComponent},
 ];
 
 @NgModule({
