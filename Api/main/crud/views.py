34ba679b0274ea88162f6f2e97fe60732038297b
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 12:30:07 +0200

    add ListView in views.py

--- Api/main/crud/views.py
+++ Api/main/crud/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import List
+from .serializers import ListSerializer
+class ListView(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
