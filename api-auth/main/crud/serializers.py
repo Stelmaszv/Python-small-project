003be06dbc267520128c5d67b49625d4e94783e9
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 12:59:06 +0200

    api auth init

---
+++ api-auth/main/crud/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import List
+class ListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model  = List
+        fields =  ('id','name','url')
---
+++ api-auth/main/crud/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import List
+from .serializers import ListSerializer
+class ListView(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
