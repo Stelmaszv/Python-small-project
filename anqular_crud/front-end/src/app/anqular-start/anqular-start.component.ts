4cd860655f9c5a5ca34cf863721ee6c5b7b950ce
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 15:14:43 +0200

    add create component

--- anqular_crud/front-end/src/app/anqular-start/anqular-start.component.ts
+++ anqular_crud/front-end/src/app/anqular-start/anqular-start.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anqular-start.component.sass']
 })
 export class AnqularStartComponent implements OnInit {
-
+  title = 'front-end';
   constructor() { }
 
   ngOnInit(): void {
--- anqular_crud/front-end/src/app/app-routing.module.ts
+++ anqular_crud/front-end/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent } from './base/base.component';
 import { AnqularStartComponent } from './anqular-start/anqular-start.component';
+import { ListComponent } from './list/list.component'
+import { CreateComponent } from './create/create.component';
 const routes: Routes = [
-  { path:'AnqularStart' , component:AnqularStartComponent },
-  { path:'' , component:BaseComponent}
+  { path:'' , component:ListComponent},
+  { path:'create' , component: CreateComponent},
 ];
 
 @NgModule({
