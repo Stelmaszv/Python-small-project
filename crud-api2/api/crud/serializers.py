8fe4dda361a7166b83e39007af22d3e36b72614e
Stelmaszv <stelmaszv@gmail.com>
2020-05-20 12:32:56 +0200

    removed old api-crud

--- crud-api2/api/crud/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import List
-class ListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model  = List
-        fields =  ('id','name','url')
